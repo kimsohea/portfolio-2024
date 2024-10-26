5ff16b7802c4e34b3d07a68123b5e848fafd701c
kimsohea <kimsohea4425@gmail.com>
2024-10-26 20:33:10 +0900

    fucntion: 화면 너비에 따른 스크롤 기능 변경

--- src/main.js
+++ src/main.js
@@ -15,18 +15,37 @@
     let imgFlg = false;
 
     for (let num = 0; num < 10; num++) {
-      scrollArr.push(scrollState * num + winHeight);
+      if (window.innerWidth > 900) {
+        scrollArr.push(scrollState * num + winHeight);
+      } else {
+        scrollArr.push(scrollState * num + siteSec.offsetTop);
+      }
     }
+
+    document.querySelector(".dummy").style.height =
+      scrollArr[9] - introTop + 150 + "px";
 
     window.addEventListener("scroll", function () {
       let scroll = this.scrollY;
 
-      if (winHeight < scroll && scroll > introTop && scroll < scrollArr[9]) {
-        let cssTop = scroll - introTop + 20;
-        siteSec.style.transform = "translateY(" + cssTop + "px)";
-        siteSec.classList.add("active");
-      } else if (scroll < scrollArr[0]) {
-        siteSec.classList.remove("active");
+      if (window.innerWidth > 900) {
+        if (winHeight < scroll && scroll > introTop && scroll < scrollArr[9]) {
+          let cssTop = scroll - introTop;
+          siteSec.style.transform = "translateY(" + cssTop + "px)";
+          siteSec.classList.add("active");
+        } else if (scroll < scrollArr[0]) {
+          siteSec.classList.remove("active");
+          siteSec.style.transform = "translateY(0px)";
+        }
+      } else {
+        if (siteSec.offsetTop < scroll && scroll < scrollArr[9]) {
+          let cssTop = scroll - siteSec.offsetTop;
+          siteSec.style.transform = "translateY(" + cssTop + "px)";
+          siteSec.classList.add("active");
+        } else if (scroll < scrollArr[0]) {
+          siteSec.classList.remove("active");
+          siteSec.style.transform = "translateY(0px)";
+        }
       }
 
       scrollArr.forEach((state, idx) => {
@@ -41,12 +60,13 @@
           siteInfo[5].setAttribute("href", textArr[idx].siteMo);
 
           siteDeploy.setAttribute("src", textArr[idx].imgSrc);
-          setTimeout(() => (imgFlg = true), 500);
+          imgFlg = true;
 
           if (idx > 5) {
-            siteInfo[5].innerText = "사이트 바로가기";
+            siteInfo[4].innerText = "사이트 바로가기";
             siteInfo[5].style.display = "none";
           } else {
+            siteInfo[4].innerText = "PC 사이트 바로가기";
             siteInfo[5].style.display = "block";
           }
 
@@ -57,6 +77,13 @@
 
         if (imgFlg) siteDeploy.classList.add("active");
         else siteDeploy.classList.remove("active");
+      });
+    });
+
+    siteIcons.forEach((item, idx) => {
+      item.addEventListener("click", () => {
+        console.log(item.querySelector("button img").attributes[0]);
+        window.scrollTo({ top: scrollArr[idx + 1], behavior: "smooth" });
       });
     });
   });
